2d0754d071584ba4c6bf999c5ae11e6f9ca25e9d
ojasvinaik13 <ojasvi.naik@gmail.com>
2020-11-28 16:12:55 +0530

    Minor Change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SlotsComponent } from './slots/slots.component';
+
 
 
 const routes: Routes = [
-  { path: 'slots', component: SlotsComponent },
+  
 ];
 
 @NgModule({
